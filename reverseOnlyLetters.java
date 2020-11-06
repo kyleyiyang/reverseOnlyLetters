1b4a06774a9df3f03857072a74249ec83994df1b
kyleyiyang <kyleyiyang@gmail.com>
2020-11-05 22:37:46 -0500

    Update reverseOnlyLetters.java

--- reverseOnlyLetters.java
+++ reverseOnlyLetters.java
@@ -2,21 +2,20 @@ class Solution {
     public String reverseOnlyLetters(String S) {
         String out="";
         int j=0;
-        int count=0;
         for (int i=S.length()-1;i>=0;i--) {
-            if (j<=S.length()-1-i) j=S.length()-1-i;
-            //else j+=count;
-            while (!Character.isLetter(S.charAt(j))) {
-                System.out.println("i="+i);
-                System.out.println("j="+j+"; "+S.charAt(j));
+            while (j<S.length() && !Character.isLetter(S.charAt(j))) {
                 out+=S.charAt(j);
-                System.out.println("j="+j+"; "+out);
                 j++;
             }
-            //System.out.println("j="+j+"; "+S.charAt(j));
-            if (Character.isLetter(S.charAt(i))) {
+            if (Character.isLetter(S.charAt(i)) && j<S.length() && Character.isLetter(S.charAt(j))) {
                 out+=S.charAt(i);
-                count++;
+                j++;
+            }
+        }
+        if (j<S.length()) {
+            while (j<S.length() && !Character.isLetter(S.charAt(j))) {
+                out+=S.charAt(j);
+                j++;
             }
         }
         return out;
